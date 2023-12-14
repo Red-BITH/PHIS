01c1043f52ece29ee104aa065c29bc18f5a0dd8f
RED BİTH <136892734+Red-BITH@users.noreply.github.com>
2023-12-14 07:52:32 -0800

    Create start.py

---
+++ start.py
@@ -0,0 +1,11 @@
+import os
+print("\033[34mbu dosyayi bir kere kullanmainz yeterlidir!!!")
+os.system("clear")
+os.system("chmod +x slobit.py")
+os.system("pip install requests")
+os.system("clear")
+print("""\033[32m
+###############################################################################
+#HER SEY TAMAM! SADECE "python slophis.py" diyerek toolu calistira bilirsiniz!#
+###############################################################################
+""")
